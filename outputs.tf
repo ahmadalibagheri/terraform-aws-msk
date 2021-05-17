bc1b6d031472aaa8c4d467014139540a81638921
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-05-17 07:23:17 +0100

    update outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,53 @@
+################################################################################
+# Cluster
+################################################################################
+
+output "arn" {
+  description = "Amazon Resource Name (ARN) of the MSK cluster"
+  value       = try(aws_msk_cluster.this[0].arn, "")
+}
+
+output "bootstrap_brokers" {
+  description = "Comma separated list of one or more hostname:port pairs of kafka brokers suitable to bootstrap connectivity to the kafka cluster"
+  value = compact([
+    try(aws_msk_cluster.this[0].bootstrap_brokers, ""),
+    try(aws_msk_cluster.this[0].bootstrap_brokers_sasl_iam, ""),
+    try(aws_msk_cluster.this[0].bootstrap_brokers_sasl_scram, ""),
+    try(aws_msk_cluster.this[0].bootstrap_brokers_tls, ""),
+  ])
+}
+
+output "bootstrap_brokers_plaintext" {
+  description = "Comma separated list of one or more hostname:port pairs of kafka brokers suitable to bootstrap connectivity to the kafka cluster. Contains a value if `encryption_in_transit_client_broker` is set to `PLAINTEXT` or `TLS_PLAINTEXT`"
+  value       = try(aws_msk_cluster.this[0].bootstrap_brokers, "")
+}
+
+output "bootstrap_brokers_sasl_iam" {
+  description = "One or more DNS names (or IP addresses) and SASL IAM port pairs. This attribute will have a value if `encryption_in_transit_client_broker` is set to `TLS_PLAINTEXT` or `TLS` and `client_authentication_sasl_iam` is set to `true`"
+  value       = try(aws_msk_cluster.this[0].bootstrap_brokers_sasl_iam, "")
+}
+
+output "bootstrap_brokers_sasl_scram" {
+  description = "One or more DNS names (or IP addresses) and SASL SCRAM port pairs. This attribute will have a value if `encryption_in_transit_client_broker` is set to `TLS_PLAINTEXT` or `TLS` and `client_authentication_sasl_scram` is set to `true`"
+  value       = try(aws_msk_cluster.this[0].bootstrap_brokers_sasl_scram, "")
+}
+
+output "bootstrap_brokers_tls" {
+  description = "One or more DNS names (or IP addresses) and TLS port pairs. This attribute will have a value if `encryption_in_transit_client_broker` is set to `TLS_PLAINTEXT` or `TLS`"
+  value       = try(aws_msk_cluster.this[0].bootstrap_brokers_tls, "")
+}
+
+output "current_version" {
+  description = "Current version of the MSK Cluster used for updates, e.g. `K13V1IB3VIYZZH`"
+  value       = try(aws_msk_cluster.this[0].current_version, "")
+}
+
+output "zookeeper_connect_string" {
+  description = "A comma separated list of one or more hostname:port pairs to use to connect to the Apache Zookeeper cluster. The returned values are sorted alphabetically"
+  value       = try(aws_msk_cluster.this[0].zookeeper_connect_string, "")
+}
+
+output "zookeeper_connect_string_tls" {
+  description = "A comma separated list of one or more hostname:port pairs to use to connect to the Apache Zookeeper cluster via TLS. The returned values are sorted alphabetically"
+  value       = try(aws_msk_cluster.this[0].zookeeper_connect_string_tls, "")
+}
