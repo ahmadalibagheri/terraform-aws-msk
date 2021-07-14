8c076a54ee0f4a20a7ad3ee49c2f7d1d51097611
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-07-14 14:09:23 +0100

    update msk dashboard

--- msk-dashboard/main.tf
+++ msk-dashboard/main.tf
@@ -1,133 +1,153 @@
-resource "aws_cloudwatch_log_group" "kafka_logging" {
-  name = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
-  retention_in_days = 30
+variable "infra_data_s3_name"   {}
+variable "infra_data_s3_region" {}
+variable "infra_data_s3_key"    {}
+variable "enabled"              { default = true }
+variable "host_name"            { default = "kafka-ui" }
+variable "eks_name"             {}
+variable "security_groups"      {}
+variable "kafka_bootstap"       {}
+variable "replicas"             { default = "1" }
+variable "public_ingress"       { default = true }
+variable "namespace"            { default = "default" }
+variable "kafka_ui_image"       { default = "0.2.1-20211215114334" }
+
+data "terraform_remote_state" "infra" {
+    backend = "s3"
+    config  = {
+        bucket     = var.infra_data_s3_name
+        key        = var.infra_data_s3_key
+        region     = var.infra_data_s3_region
+    }
 }
 
-################################################################################
-# Configuration
-################################################################################
-
-resource "aws_msk_configuration" "kafka_configuration" {
-  count = var.create ? 1 : 0
-  
-  name              = coalesce(var.configuration_name, var.name)
-  description       = var.configuration_description
-  kafka_versions = [var.kafka_version]
-  server_properties = join("\n", local.server_properties)
+provider "aws" {
+    region = "${data.terraform_remote_state.infra.outputs.aws_region}"
 }
 
-locals {
-  server_properties = [
-    "num.partitions=${lookup(var.kafka_config, "num.partitions", "15")}",
-    "default.replication.factor=${lookup(var.kafka_config, "default.replication.factor", "3")}",
-    "auto.create.topics.enable=${lookup(var.kafka_config, "auto.create.topics.enable", "true")}",
-    "delete.topic.enable=${lookup(var.kafka_config, "delete.topic.enable", "true")}"
-  ]
+data "aws_eks_cluster" "eks" {
+    name = var.eks_name
 }
 
-################################################################################
-# MSK Cluster
-################################################################################
+data "aws_eks_cluster_auth" "eks" {
+    name = var.eks_name
+}
 
-resource "aws_msk_cluster" "kafka_cluster" {
-  cluster_name = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
-  kafka_version = var.kafka_version
-  number_of_broker_nodes = var.instance_count
+provider "kubernetes" {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+}
 
-  broker_node_group_info {
-    instance_type = var.instance_type
-    ebs_volume_size = var.kafka_data_disk
-    client_subnets = length(var.subnets) > 0 ? var.subnets : data.terraform_remote_state.infra.outputs.subnet_ids
-    security_groups = length(var.security_groups) > 0 ? var.security_groups : [data.terraform_remote_state.infra.outputs.default_sg_id]
-  }
-
-  configuration_info {
-    arn = aws_msk_configuration.kafka_configuration.arn
-    revision = aws_msk_configuration.kafka_configuration.latest_revision
-  }
-
-  encryption_info {
-    encryption_in_transit {
-      client_broker = var.client_broker_encryption
-      in_cluster = true
+resource "kubernetes_service" "kafka-ui" {
+  count = "${var.enabled ? 1 : 0}"  
+  metadata {
+    name = "${var.host_name}"
+    namespace = "${var.namespace}"
+    labels = {
+      app = "${var.host_name}"
     }
   }
 
-  enhanced_monitoring = var.monitoring_type
+  spec {
+    port {
+      name        = "http"
+      port        = 8080
+      target_port = "8080"
+    }
 
-  logging_info {
-    broker_logs {
-      cloudwatch_logs {
-        enabled = true
-        log_group = aws_cloudwatch_log_group.kafka_logging.name
+    selector = {
+      app = "${var.host_name}"
+    }
+
+    type = "NodePort"
+  }
+}
+
+
+resource "kubernetes_deployment" "kafka-ui" {
+  count = "${var.enabled ? 1 : 0}"
+  metadata {
+    name      = "${var.host_name}"
+    namespace = "${var.namespace}"
+  }
+
+  spec {
+    replicas = "${var.replicas}"
+
+    selector {
+      match_labels = {
+        app = "${var.host_name}"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "${var.host_name}"
+        }
+      }
+
+      spec {
+        container {
+          name  = "${var.host_name}"
+          image = "${data.terraform_remote_state.infra.outputs.owner_id}.dkr.ecr.${data.terraform_remote_state.infra.outputs.aws_region}.amazonaws.com/kafkauiimage:${var.kafka_ui_image}"
+
+          port {
+            name           = "http"
+            container_port = 8080
+            protocol       = "TCP"
+          }
+
+          env {
+            name  = "KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS"
+            value = "${var.kafka_bootstap}"
+          }
+
+          env {
+            name  = "KAFKA_CLUSTERS_0_NAME"
+            value = "local"
+          }
+
+        }
+        node_selector = {
+          "kubernetes.io/role" = "node"
+        }
       }
     }
   }
-
-  tags = {
-    Name = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
-    Environment = data.terraform_remote_state.infra.outputs.environment_name
-  }
 }
 
-################################################################################
-# CloudWatch Log Group
-################################################################################
+resource "kubernetes_ingress" "kafka-ui_internal" {
+  count = "${var.public_ingress ? 1 : 0}"
+  metadata {
+    name = "${var.host_name}"
+    namespace = "${var.namespace}"
+    annotations = {
+      "alb.ingress.kubernetes.io/group.name" = "default"
 
-resource "aws_cloudwatch_log_group" "this" {
-  count = var.create && var.create_cloudwatch_log_group ? 1 : 0
+      "alb.ingress.kubernetes.io/listen-ports" = "[{\"HTTPS\": 443}]"
 
-  name              = coalesce(var.cloudwatch_log_group_name, "/aws/msk/${var.name}")
-  retention_in_days = var.cloudwatch_log_group_retention_in_days
-  kms_key_id        = var.cloudwatch_log_group_kms_key_id
+      "alb.ingress.kubernetes.io/scheme" = "internal"
 
-  tags = var.tags
-}
+      "alb.ingress.kubernetes.io/security-groups" = "${var.security_groups}"
 
-################################################################################
-# Connect Custom Plugin
-################################################################################
+      "alb.ingress.kubernetes.io/tags" = "${var.host_name}"
 
-resource "aws_mskconnect_custom_plugin" "this" {
-  for_each = { for k, v in var.connect_custom_plugins : k => v if var.create }
-
-  name         = each.value.name
-  description  = lookup(each.value, "description", null)
-  content_type = each.value.content_type
-
-  location {
-    s3 {
-      bucket_arn     = each.value.s3_bucket_arn
-      file_key       = each.value.s3_file_key
-      object_version = lookup(each.value, "s3_object_version", null)
+      "kubernetes.io/ingress.class" = "alb"
     }
   }
+  spec {
+    rule {
+      host = "${var.host_name}.${data.terraform_remote_state.infra.outputs.route53_zone_name}"
 
-  timeouts {
-    create = try(each.value.timeouts.create, var.connect_custom_plugin_timeouts.create, null)
+      http {
+        path {
+          backend {
+            service_name = "${var.host_name}"
+            service_port = "8080"
+          }
+        }
+      }
+    }
   }
 }
-
-################################################################################
-# Connect Worker Configuration
-################################################################################
-
-resource "aws_mskconnect_worker_configuration" "this" {
-  count = var.create && var.create_connect_worker_configuration ? 1 : 0
-
-  name                    = var.connect_worker_config_name
-  description             = var.connect_worker_config_description
-  properties_file_content = var.connect_worker_config_properties_file_content
-}
-
-resource "aws_route53_record" "kafka_dns_record" {
-  for_each = var.no_dns ? {} : { for v in toset(split(",", replace(aws_msk_cluster.kafka_cluster.bootstrap_brokers, ":9092", ""))): v => v }
-  zone_id  = data.terraform_remote_state.infra.outputs.route53_zone_id
-  name     = "${lower(var.name)}-broker${split("-", split(".", each.value)[0])[1]}.${data.terraform_remote_state.infra.outputs.route53_zone_name}"
-  type     = "CNAME"
-  ttl      = "30"
-  records  = [ each.value ]
-}
-
-
-
