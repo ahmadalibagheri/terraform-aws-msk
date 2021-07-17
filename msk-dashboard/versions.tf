87797a07116dfef10caa616d916a1f517d0a4e9b
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-07-17 14:13:35 +0100

    update msk dashboard version

--- msk-dashboard/versions.tf
+++ msk-dashboard/versions.tf
@@ -8,3 +8,21 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+    region = "${data.terraform_remote_state.infra.outputs.aws_region}"
+}
+
+data "aws_eks_cluster" "eks" {
+    name = var.eks_name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+    name = var.eks_name
+}
+
+provider "kubernetes" {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+}
