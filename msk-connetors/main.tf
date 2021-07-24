8f7a1cecd77b43d0773fc04c23561172a0c51da5
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-07-24 14:17:45 +0100

    fmt

--- msk-connetors/main.tf
+++ msk-connetors/main.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "kafka_logging" {
-  name = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
+  name              = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
   retention_in_days = 30
 }
 
@@ -9,10 +9,10 @@ resource "aws_cloudwatch_log_group" "kafka_logging" {
 
 resource "aws_msk_configuration" "kafka_configuration" {
   count = var.create ? 1 : 0
-  
+
   name              = coalesce(var.configuration_name, var.name)
   description       = var.configuration_description
-  kafka_versions = [var.kafka_version]
+  kafka_versions    = [var.kafka_version]
   server_properties = join("\n", local.server_properties)
 }
 
@@ -30,26 +30,26 @@ locals {
 ################################################################################
 
 resource "aws_msk_cluster" "kafka_cluster" {
-  cluster_name = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
-  kafka_version = var.kafka_version
+  cluster_name           = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
+  kafka_version          = var.kafka_version
   number_of_broker_nodes = var.instance_count
 
   broker_node_group_info {
-    instance_type = var.instance_type
+    instance_type   = var.instance_type
     ebs_volume_size = var.kafka_data_disk
-    client_subnets = length(var.subnets) > 0 ? var.subnets : data.terraform_remote_state.infra.outputs.subnet_ids
+    client_subnets  = length(var.subnets) > 0 ? var.subnets : data.terraform_remote_state.infra.outputs.subnet_ids
     security_groups = length(var.security_groups) > 0 ? var.security_groups : [data.terraform_remote_state.infra.outputs.default_sg_id]
   }
 
   configuration_info {
-    arn = aws_msk_configuration.kafka_configuration.arn
+    arn      = aws_msk_configuration.kafka_configuration.arn
     revision = aws_msk_configuration.kafka_configuration.latest_revision
   }
 
   encryption_info {
     encryption_in_transit {
       client_broker = var.client_broker_encryption
-      in_cluster = true
+      in_cluster    = true
     }
   }
 
@@ -58,14 +58,14 @@ resource "aws_msk_cluster" "kafka_cluster" {
   logging_info {
     broker_logs {
       cloudwatch_logs {
-        enabled = true
+        enabled   = true
         log_group = aws_cloudwatch_log_group.kafka_logging.name
       }
     }
   }
 
   tags = {
-    Name = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
+    Name        = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
     Environment = data.terraform_remote_state.infra.outputs.environment_name
   }
 }
@@ -121,12 +121,12 @@ resource "aws_mskconnect_worker_configuration" "this" {
 }
 
 resource "aws_route53_record" "kafka_dns_record" {
-  for_each = var.no_dns ? {} : { for v in toset(split(",", replace(aws_msk_cluster.kafka_cluster.bootstrap_brokers, ":9092", ""))): v => v }
+  for_each = var.no_dns ? {} : { for v in toset(split(",", replace(aws_msk_cluster.kafka_cluster.bootstrap_brokers, ":9092", ""))) : v => v }
   zone_id  = data.terraform_remote_state.infra.outputs.route53_zone_id
   name     = "${lower(var.name)}-broker${split("-", split(".", each.value)[0])[1]}.${data.terraform_remote_state.infra.outputs.route53_zone_name}"
   type     = "CNAME"
   ttl      = "30"
-  records  = [ each.value ]
+  records  = [each.value]
 }
 
 
