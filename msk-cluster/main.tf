a90028b6144d17517e08543c9d51d353e55b7821
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-07-28 18:58:54 +0100

    update msk config

--- msk-cluster/main.tf
+++ msk-cluster/main.tf
@@ -1,14 +1,9 @@
-resource "aws_cloudwatch_log_group" "kafka_logging" {
-  name              = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
-  retention_in_days = 30
-}
-
 ################################################################################
 # Configuration
 ################################################################################
 
-resource "aws_msk_configuration" "kafka_configuration" {
-  count = var.create ? 1 : 0
+resource "aws_msk_configuration" "this" {
+  count = var.create_msk_configuration ? 1 : 0
 
   name              = coalesce(var.configuration_name, var.name)
   description       = var.configuration_description
@@ -25,61 +20,61 @@ locals {
   ]
 }
 
-################################################################################
-# MSK Cluster
-################################################################################
+# ################################################################################
+# # MSK Cluster
+# ################################################################################
 
-resource "aws_msk_cluster" "kafka_cluster" {
-  cluster_name           = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
-  kafka_version          = var.kafka_version
-  number_of_broker_nodes = var.instance_count
+# resource "aws_msk_cluster" "kafka_cluster" {
+#   cluster_name           = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
+#   kafka_version          = var.kafka_version
+#   number_of_broker_nodes = var.instance_count
 
-  broker_node_group_info {
-    instance_type   = var.instance_type
-    ebs_volume_size = var.kafka_data_disk
-    client_subnets  = length(var.subnets) > 0 ? var.subnets : data.terraform_remote_state.infra.outputs.subnet_ids
-    security_groups = length(var.security_groups) > 0 ? var.security_groups : [data.terraform_remote_state.infra.outputs.default_sg_id]
-  }
+#   broker_node_group_info {
+#     instance_type   = var.instance_type
+#     ebs_volume_size = var.kafka_data_disk
+#     client_subnets  = length(var.subnets) > 0 ? var.subnets : data.terraform_remote_state.infra.outputs.subnet_ids
+#     security_groups = length(var.security_groups) > 0 ? var.security_groups : [data.terraform_remote_state.infra.outputs.default_sg_id]
+#   }
 
-  configuration_info {
-    arn      = aws_msk_configuration.kafka_configuration.arn
-    revision = aws_msk_configuration.kafka_configuration.latest_revision
-  }
+#   configuration_info {
+#     arn      = aws_msk_configuration.kafka_configuration.arn
+#     revision = aws_msk_configuration.kafka_configuration.latest_revision
+#   }
 
-  encryption_info {
-    encryption_in_transit {
-      client_broker = var.client_broker_encryption
-      in_cluster    = true
-    }
-  }
+#   encryption_info {
+#     encryption_in_transit {
+#       client_broker = var.client_broker_encryption
+#       in_cluster    = true
+#     }
+#   }
 
-  enhanced_monitoring = var.monitoring_type
+#   enhanced_monitoring = var.monitoring_type
 
-  logging_info {
-    broker_logs {
-      cloudwatch_logs {
-        enabled   = true
-        log_group = aws_cloudwatch_log_group.kafka_logging.name
-      }
-    }
-  }
+#   logging_info {
+#     broker_logs {
+#       cloudwatch_logs {
+#         enabled   = true
+#         log_group = aws_cloudwatch_log_group.kafka_logging.name
+#       }
+#     }
+#   }
 
-  tags = {
-    Name        = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
-    Environment = data.terraform_remote_state.infra.outputs.environment_name
-  }
-}
+#   tags = {
+#     Name        = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
+#     Environment = data.terraform_remote_state.infra.outputs.environment_name
+#   }
+# }
 
-################################################################################
-# CloudWatch Log Group
-################################################################################
+# ################################################################################
+# # CloudWatch Log Group
+# ################################################################################
 
-resource "aws_cloudwatch_log_group" "this" {
-  count = var.create && var.create_cloudwatch_log_group ? 1 : 0
+# resource "aws_cloudwatch_log_group" "this" {
+#   count = var.create && var.create_cloudwatch_log_group ? 1 : 0
 
-  name              = coalesce(var.cloudwatch_log_group_name, "/aws/msk/${var.name}")
-  retention_in_days = var.cloudwatch_log_group_retention_in_days
-  kms_key_id        = var.cloudwatch_log_group_kms_key_id
+#   name              = coalesce(var.cloudwatch_log_group_name, "/aws/msk/${var.name}")
+#   retention_in_days = var.cloudwatch_log_group_retention_in_days
+#   kms_key_id        = var.cloudwatch_log_group_kms_key_id
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
