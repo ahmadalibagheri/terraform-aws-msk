df0a17d31eb7c43b41b8d2b4552ea9187e478847
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-08-03 12:42:37 +0100

    update cloud watch

--- msk-cluster/main.tf
+++ msk-cluster/main.tf
@@ -70,7 +70,7 @@ locals {
 ################################################################################
 
 resource "aws_cloudwatch_log_group" "this" {
-  count = var.create && var.create_cloudwatch_log_group ? 1 : 0
+  count = var.create_cloudwatch_log_group ? 1 : 0
 
   name              = coalesce(var.cloudwatch_log_group_name, "/aws/msk/${var.name}")
   retention_in_days = var.cloudwatch_log_group_retention_in_days
