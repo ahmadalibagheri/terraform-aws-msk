08d4c17fb69a5ccddfe2d3e51e056e1cf71d7bef
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-08-03 12:34:01 +0100

    update main

--- msk-cluster/main.tf
+++ msk-cluster/main.tf
@@ -65,16 +65,16 @@ locals {
 #   }
 # }
 
-# ################################################################################
-# # CloudWatch Log Group
-# ################################################################################
+################################################################################
+# CloudWatch Log Group
+################################################################################
 
-# resource "aws_cloudwatch_log_group" "this" {
-#   count = var.create && var.create_cloudwatch_log_group ? 1 : 0
+resource "aws_cloudwatch_log_group" "this" {
+  count = var.create && var.create_cloudwatch_log_group ? 1 : 0
 
-#   name              = coalesce(var.cloudwatch_log_group_name, "/aws/msk/${var.name}")
-#   retention_in_days = var.cloudwatch_log_group_retention_in_days
-#   kms_key_id        = var.cloudwatch_log_group_kms_key_id
+  name              = coalesce(var.cloudwatch_log_group_name, "/aws/msk/${var.name}")
+  retention_in_days = var.cloudwatch_log_group_retention_in_days
+  kms_key_id        = var.cloudwatch_log_group_kms_key_id
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
