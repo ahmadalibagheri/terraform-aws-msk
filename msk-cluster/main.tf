0cbf5a335a7c341959047b40707b4df03c0f1dc0
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-08-04 12:48:56 +0100

    add msk cluster

--- msk-cluster/main.tf
+++ msk-cluster/main.tf
@@ -24,46 +24,79 @@ locals {
 # # MSK Cluster
 # ################################################################################
 
-# resource "aws_msk_cluster" "kafka_cluster" {
-#   cluster_name           = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
-#   kafka_version          = var.kafka_version
-#   number_of_broker_nodes = var.instance_count
+resource "aws_msk_cluster" "this" {
+  count = var.create_kafka_cluster ? 1 : 0
 
-#   broker_node_group_info {
-#     instance_type   = var.instance_type
-#     ebs_volume_size = var.kafka_data_disk
-#     client_subnets  = length(var.subnets) > 0 ? var.subnets : data.terraform_remote_state.infra.outputs.subnet_ids
-#     security_groups = length(var.security_groups) > 0 ? var.security_groups : [data.terraform_remote_state.infra.outputs.default_sg_id]
-#   }
+  cluster_name           = var.name
+  kafka_version          = var.kafka_version
+  number_of_broker_nodes = var.instance_count
+  enhanced_monitoring    = var.enhanced_monitoring
 
-#   configuration_info {
-#     arn      = aws_msk_configuration.kafka_configuration.arn
-#     revision = aws_msk_configuration.kafka_configuration.latest_revision
-#   }
+  broker_node_group_info {
+    client_subnets  = var.broker_node_client_subnets
+    ebs_volume_size = var.broker_node_ebs_volume_size
+    instance_type   = var.broker_node_instance_type
+    security_groups = var.broker_node_security_groups
+  }
 
-#   encryption_info {
-#     encryption_in_transit {
-#       client_broker = var.client_broker_encryption
-#       in_cluster    = true
-#     }
-#   }
+  configuration_info {
+    arn      = aws_msk_configuration.this[0].arn
+    revision = aws_msk_configuration.this[0].latest_revision
+  }
 
-#   enhanced_monitoring = var.monitoring_type
+  encryption_info {
+    encryption_in_transit {
+      client_broker = var.encryption_in_transit_client_broker
+      in_cluster    = var.encryption_in_transit_in_cluster
+    }
+    encryption_at_rest_kms_key_arn = var.encryption_at_rest_kms_key_arn
+  }
 
-#   logging_info {
-#     broker_logs {
-#       cloudwatch_logs {
-#         enabled   = true
-#         log_group = aws_cloudwatch_log_group.kafka_logging.name
-#       }
-#     }
-#   }
+  open_monitoring {
+    prometheus {
+      jmx_exporter {
+        enabled_in_broker = var.jmx_exporter_enabled
+      }
+      node_exporter {
+        enabled_in_broker = var.node_exporter_enabled
+      }
+    }
+  }
 
-#   tags = {
-#     Name        = "${data.terraform_remote_state.infra.outputs.environment_name}-${var.name}"
-#     Environment = data.terraform_remote_state.infra.outputs.environment_name
-#   }
-# }
+  logging_info {
+    broker_logs {
+      cloudwatch_logs {
+        enabled   = var.cloudwatch_logs_enabled
+        log_group = var.cloudwatch_logs_enabled ? local.cloudwatch_log_group : null
+      }
+      firehose {
+        enabled         = var.firehose_logs_enabled
+        delivery_stream = var.firehose_delivery_stream
+      }
+      s3 {
+        enabled = var.s3_logs_enabled
+        bucket  = var.s3_logs_bucket
+        prefix  = var.s3_logs_prefix
+      }
+    }
+  }
+
+  timeouts {
+    create = lookup(var.timeouts, "create", null)
+    update = lookup(var.timeouts, "update", null)
+    delete = lookup(var.timeouts, "delete", null)
+  }
+
+  # required for appautoscaling
+  lifecycle {
+    ignore_changes = [broker_node_group_info[0].ebs_volume_size]
+  }
+
+  tags = {
+    Name        = var.name
+    Environment = var.environment
+  }
+}
 
 ################################################################################
 # CloudWatch Log Group
