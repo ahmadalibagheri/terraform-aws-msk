87d87d2cb560d25407292dcc0a6da2a8f3fbe38b
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-08-15 13:50:23 +0100

    update msk cluster

--- main.tf
+++ main.tf
@@ -1,4 +1,28 @@
 module "kafka-cluster" {
   source = "./msk-cluster"
   name   = "Kafka"
+  number_of_broker_nodes = 3
+  enhanced_monitoring    = "PER_TOPIC_PER_PARTITION"
+
+  broker_node_client_subnets  = ["subnet-12345678", "subnet-024681012", "subnet-87654321"]
+  broker_node_ebs_volume_size = 20
+  broker_node_instance_type   = "kafka.t3.small"
+  broker_node_security_groups = ["sg-12345678"]
+
+  encryption_in_transit_client_broker = "TLS"
+  encryption_in_transit_in_cluster    = true
+
+  configuration_name        = "example-configuration"
+  configuration_description = "Example configuration"
+  configuration_server_properties = {
+    "auto.create.topics.enable" = true
+    "delete.topic.enable"       = true
+  }
+
+  jmx_exporter_enabled    = true
+  node_exporter_enabled   = true
+  cloudwatch_logs_enabled = true
+  s3_logs_enabled         = true
+  s3_logs_bucket          = "aws-msk-kafka-cluster-logs"
+
 }
