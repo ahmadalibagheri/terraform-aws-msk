9c2d782c94109844d55a4f207eff8ff88076b72a
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-08-21 13:49:39 +0100

    update main

--- main.tf
+++ main.tf
@@ -3,11 +3,12 @@ module "kafka-cluster" {
   name                   = "Kafka"
   number_of_broker_nodes = 3
   enhanced_monitoring    = "PER_TOPIC_PER_PARTITION"
+  environment = "dev"
 
-  broker_node_client_subnets  = ["subnet-12345678", "subnet-024681012", "subnet-87654321"]
+  broker_node_client_subnets  = ["subnet-a", "subnet-b", "subnet-c"] #Adding your subnet
   broker_node_ebs_volume_size = 20
   broker_node_instance_type   = "kafka.t3.small"
-  broker_node_security_groups = ["sg-12345678"]
+  broker_node_security_groups = ["sg-default"] #Adding your security group
 
   encryption_in_transit_client_broker = "TLS"
   encryption_in_transit_in_cluster    = true
@@ -22,7 +23,7 @@ module "kafka-cluster" {
   jmx_exporter_enabled    = true
   node_exporter_enabled   = true
   cloudwatch_logs_enabled = true
-  s3_logs_enabled         = true
-  s3_logs_bucket          = "aws-msk-kafka-cluster-logs"
+  # s3_logs_enabled         = true
+  # s3_logs_bucket          = "S3-bucket-msk-log"
 
 }
