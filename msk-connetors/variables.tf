09d5e766929ae8b35b799103890be6a78719bfb7
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2022-02-15 17:23:55 +0000

    update msk connect

--- msk-connetors/variables.tf
+++ msk-connetors/variables.tf
@@ -1,22 +1,3 @@
-variable "infra_data_s3_name" {}
-variable "infra_data_s3_region" {}
-variable "infra_data_s3_key" {}
-
-variable "name" {}
-variable "kafka_version" { default = "2.2.1" }
-variable "instance_count" { default = 3 }
-variable "instance_type" {}
-variable "kafka_data_disk" { default = "10" }
-variable "subnets" { default = [] }
-variable "security_groups" { default = [] }
-variable "client_broker_encryption" { default = "PLAINTEXT" }
-variable "monitoring_type" { default = "PER_BROKER" }
-variable "kafka_config" { default = {} }
-
-# Initial creation needs to run with 'no_dns = true'
-# as terraform fails to create for_each resources
-variable "no_dns" { default = false }
-
 variable "create" {
   description = "Determines whether cluster resources will be created"
   type        = bool
